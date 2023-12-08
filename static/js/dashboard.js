2348b2ef33d037f4280e97ef0a4643ecf58e3f48
Annabrabender <annabrabender1@gmail.com>
2023-12-07 20:51:46 -0500

    dec

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -12,6 +12,7 @@ function init() {
         // Assuming data is an object with a property 'VaccinationData'
         let countries = data.VaccinationData.map(item => item[0]);
         let vaccination_rates = data.VaccinationData.map(item => item[4]);
+        let seconddose = data.VaccinationData.map(item => item[5]);
 
         console.log(data, countries, vaccination_rates);
 
@@ -23,6 +24,7 @@ function init() {
             dropdown.id = 'country-dropdown';
             dropdown.onchange = function() {
                 updateChart(this.value);
+                updateChart2(this.value);
             };
 
             // Add an option for all countries
@@ -49,36 +51,98 @@ function init() {
             y: vaccination_rates,
             text: vaccination_rates,
             type: "bar",
+            marker: {color:'DarkCyan'}
         };
 
         const layoutbar1 = {
-            title: `Vaccination rates`,
+            title: `Total population vaccinated per 100 people`,
             xaxis: { title: "Countries" , tickangle: -45},
             yaxis: { title: "Vaccination Rates" },
         };
 
         // Update or create the initial bar chart
         Plotly.newPlot("bar", [tracebar1], layoutbar1);
+
+        //Create intial bubble chart with all countries 
+        const tracebar2 = {
+            x: countries,
+            y: seconddose,
+            text: seconddose,
+            mode: 'markers',
+            marker:{
+                size: seconddose,
+                color: seconddose,
+                colorscale:'YlGnBu'
+            }
+        };
+
+        const layoutbar2 = {
+            title: `Population vaccinated with more than one dose per 100 people`,
+            xaxis: { title: "Countries" , tickangle: -45},
+            yaxis: { title: "Second Dose per 100" },
+        };
+
+        // Update or create the initial bar chart
+        Plotly.newPlot("bubble", [tracebar2], layoutbar2);
+
     });
 }
 
-// Function to update the chart based on the selected country
+// Function to update the bar chart based on the selected country
 function updateChart(selectedCountry) {
     d3.json(data_url).then(function (data) {
+        
         let countries = data.VaccinationData.map(item => item[0]);
         let vaccination_rates = data.VaccinationData.map(item => item[4]);
+
+        
 
         if (selectedCountry === 'all') {
             // Show all countries
             Plotly.update("bar", {x: [countries], y: [vaccination_rates]});
         } else {
+            console.log(selectedCountry);
             // Show only the selected country
             const selectedIndex = countries.indexOf(selectedCountry);
             const traceUpdate = {
-                x: [countries[selectedIndex]],
-                y: [vaccination_rates[selectedIndex]],
+                x: [[countries[selectedIndex]]],
+                y: [[vaccination_rates[selectedIndex]]],
             };
+            //Plotly.update("bar", [traceUpdate]);
+            console.log([countries[selectedIndex]]);
+            console.log([vaccination_rates[selectedIndex]]);
+            //Plotly.restyle("bar", "x", [countries[selectedIndex]]);
+            //Plotly.restyle("bar", "y", [vaccination_rates[selectedIndex]]);
+            console.log(selectedIndex);
             Plotly.update("bar", traceUpdate);
+        }
+    });
+}
+
+
+// Function to update the bubble chart based on the selected country
+function updateChart2(selectedCountry) {
+    d3.json(data_url).then(function (data) {
+        
+        let countries = data.VaccinationData.map(item => item[0]);
+        let vaccination_rates = data.VaccinationData.map(item => item[5]);
+        let seconddose = data.VaccinationData.map(item => item[5]);
+
+        
+        if (selectedCountry === 'all') {
+            // Show all countries
+            Plotly.update("bubble", {x: [countries], y: [seconddose]});
+        } else {
+            console.log(selectedCountry);
+            // Show only the selected country
+            const selectedIndex = countries.indexOf(selectedCountry);
+            const traceUpdate2 = {
+                x: [[countries[selectedIndex]]],
+                y: [[seconddose[selectedIndex]]],
+            };
+            
+            
+            Plotly.update("bubble", traceUpdate2);
         }
     });
 }
